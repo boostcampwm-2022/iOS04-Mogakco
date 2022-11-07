eea8b79360160ec41d98d1e6377a9f7984795506
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-07 11:59:21 +0900

    build: Xcode 프로젝트 생성

---
+++ Mogakco/Mogakco/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mogakco
+//
+//  Created by 김범수 on 2022/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
