27182c285ff69502008b1e49fc4d9c7e0fbc7bc4
oguuk <ogw135@gmail.com>
2022-11-14 19:25:25 +0900

    feat: ViewController 위치 재설정 및 작성

--- Mogakco/Mogakco/Presentation/Common/ViewModel/ViewController/ViewController.swift
+++ Mogakco/Mogakco/Presentation/Common/ViewModel/ViewController/ViewController.swift
@@ -5,12 +5,19 @@
 //  Created by 김범수 on 2022/11/07.
 //
 
+import RxSwift
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        layout()
+        bind()
     }
-
+    
+    func layout() {}
+    func bind() {}
 }
