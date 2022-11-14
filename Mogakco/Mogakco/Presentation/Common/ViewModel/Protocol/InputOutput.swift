42cc88efbed2848eb1142388ea7a99ef3996f683
Juhoon <wngns8334@gmail.com>
2022-11-14 18:01:38 +0900

    feat: inputoutput 프로토콜 생성

---
+++ Mogakco/Mogakco/Presentation/Common/ViewModel/Protocol/InputOutput.swift
@@ -0,0 +1,18 @@
+//
+//  InputOutput.swift
+//  Mogakco
+//
+//  Created by 이주훈 on 2022/11/14.
+//
+
+import Foundation
+import RxSwift
+
+protocol InputOutput {
+    associatedtype Input
+    associatedtype Output
+
+    var disposeBag: DisposeBag { get set }
+    func transform(input: Input) -> Output
+}
+
