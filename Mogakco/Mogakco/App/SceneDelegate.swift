e588f596f88a406c55580995cf0b360d1d68a203
sominn9 <tlsthals3415@naver.com>
2022-11-14 15:53:36 +0900

    feat: SceneDelegate에 AppCoordinator 연결

--- Mogakco/Mogakco/App/SceneDelegate.swift
+++ Mogakco/Mogakco/App/SceneDelegate.swift
@@ -10,14 +10,24 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var appCoordinator: AppCoordinatorProtocol?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let navigationController = UINavigationController()
+        navigationController.view.backgroundColor = .white
+        
         window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
-        window?.rootViewController = viewController
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+
+        appCoordinator = AppCoordinator(navigationController: navigationController)
+        appCoordinator?.start()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
