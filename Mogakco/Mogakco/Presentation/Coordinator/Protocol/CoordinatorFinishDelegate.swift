b00bef1ba63db3a9b07c8d8f9b7acf93d9dba40d
sominn9 <tlsthals3415@naver.com>
2022-11-14 15:52:57 +0900

    feat: Coordinator 프로토콜, AppCoordinator 생성

---
+++ Mogakco/Mogakco/Presentation/Coordinator/Protocol/CoordinatorFinishDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  CoordinatorFinishDelegate.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/14.
+//
+
+import Foundation
+
+protocol CoordinatorFinishDelegate: AnyObject {
+    
+    func coordinatorDidFinish(childCoordinator: Coordinator)
+}
