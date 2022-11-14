5e99d08b2fa4bb1e5c6f13decec6b66e3f616262
sominn9 <tlsthals3415@naver.com>
2022-11-15 01:07:24 +0900

    refactor: 버튼 생성 메서드 분리

--- Mogakco/Mogakco/Presentation/Common/View/SecureTextField.swift
+++ Mogakco/Mogakco/Presentation/Common/View/SecureTextField.swift
@@ -16,22 +16,34 @@ final class SecureTextField: TextField {
         super.setup()
         isSecureTextEntry = true
         
+        let button = UIButton()
+        setupButtonConfig(button: button)
+        setupButtonAction(button: button)
+        setupButtonImage(button: button)
+        
+        rightView = button
+    }
+    
+    func setupButtonConfig(button: UIButton) {
         var config = UIButton.Configuration.plain()
         config.contentInsets = .init(top: 0, leading: 16, bottom: 0, trailing: 16)
         config.baseBackgroundColor = .clear
         config.baseForegroundColor = .systemGray
-        
+        button.configuration = config
+    }
+    
+    func setupButtonAction(button: UIButton) {
         let action = UIAction { [weak self] action in
             if let button = action.sender as? UIButton {
                 button.isSelected.toggle()
                 self?.isSecureTextEntry.toggle()
             }
         }
-        
-        let button = UIButton(configuration: config, primaryAction: action)
+        button.addAction(action, for: .touchUpInside)
+    }
+    
+    func setupButtonImage(button: UIButton) {
         button.setImage(UIImage(systemName: "eye"), for: .normal)
         button.setImage(UIImage(systemName: "eye.slash"), for: .selected)
-        
-        rightView = button
     }
 }
