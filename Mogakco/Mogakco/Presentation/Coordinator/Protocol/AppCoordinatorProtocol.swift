371606f3dcfb3c9a35a6d11d567e51b47cc13c0f
sominn9 <tlsthals3415@naver.com>
2022-11-14 15:52:57 +0900

    feat: Coordinator 프로토콜, AppCoordinator 생성

---
+++ Mogakco/Mogakco/Presentation/Coordinator/Protocol/AppCoordinatorProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  AppCoordinator.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/14.
+//
+
+import Foundation
+
+protocol AppCoordinatorProtocol: Coordinator {
+    
+    func showAuthFlow()
+    func showMainFlow()
+}
