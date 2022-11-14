3706b51db9dfae1243b26c7f89b0bb399d1d68ea
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-15 00:00:24 +0900

    feat: addShadow UIView Extension 추가

---
+++ Mogakco/Mogakco/Util/Extension/UIView+Shadow.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+Shadow.swift
+//  Mogakco
+//
+//  Created by 김범수 on 2022/11/14.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addShadow(offset: CGSize, color: UIColor = .lightGray, opacity: Float = 0.35, radius: CGFloat = 3.0) {
+        layer.masksToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowOffset = offset
+        layer.shadowOpacity = opacity
+        layer.shadowRadius = radius
+    }
+}
