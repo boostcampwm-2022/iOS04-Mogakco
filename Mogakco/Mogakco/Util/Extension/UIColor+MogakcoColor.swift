20072126046afb4cbfab0ccbc45502c2445a3f90
Juhoon <wngns8334@gmail.com>
2022-11-14 16:14:32 +0900

    fix: Color 변수 이름 수정, Extension 파일명 변경

---
+++ Mogakco/Mogakco/Util/Extension/UIColor+MogakcoColor.swift
@@ -0,0 +1,35 @@
+//
+//  UIImage+.swift
+//  Mogakco
+//
+//  Created by 이주훈 on 2022/11/14.
+//
+
+import UIKit
+
+extension UIColor {
+    static let mogakcoColor = MogakcoColor()
+}
+
+struct MogakcoColor {
+    // Primary
+    let primaryDefault = UIColor(named: "PrimaryDefault")
+    let primarySecondary = UIColor(named: "PrimarySecondary")
+    
+    // Typography
+    let typographyPrimary = UIColor(named: "TypographyPrimary")
+    let typographySecondary = UIColor(named: "TypographySecondary")
+    let typopraphyDisabled = UIColor(named: "TypopraphyDisabled")
+    
+    // Semantic
+    let segmanticSuccess = UIColor(named: "SegmanticSuccess")
+    let semanticNegative = UIColor(named: "SemanticNegative")
+    let semanticDisabled = UIColor(named: "SemanticDisabled")
+    
+    // Border
+    let borderDefault = UIColor(named: "BorderDefault")
+    
+    // Background
+    let backgroundDefault = UIColor(named: "BackgroundDefault")
+    let backgroundSecondary = UIColor(named: "BackgroundSecondary")
+}
