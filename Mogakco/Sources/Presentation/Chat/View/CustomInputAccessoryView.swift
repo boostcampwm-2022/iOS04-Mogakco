0e0e580bebe8f5b77b9e8b79a7468c7c4b55fff3
oguuk <ogw135@gmail.com>
2022-11-16 18:34:51 +0900

    feat: CustomInputAccessoryView 구현

---
+++ Mogakco/Sources/Presentation/Chat/View/CustomInputAccessoryView.swift
@@ -0,0 +1,81 @@
+//
+//  CustomInputAccessoryView.swift
+//  Mogakco
+//
+//  Created by 오국원 on 2022/11/16.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+import Then
+
+final class CustomInputAccessoryView: UIView {
+    
+    private let messageInputTextView = UITextView().then {
+        $0.font = UIFont.systemFont(ofSize: 16)
+        $0.textAlignment = .left
+        $0.layer.cornerRadius = 15
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.mogakcoColor.borderDefault?.cgColor
+        $0.isScrollEnabled = false
+        
+    }
+    
+    private let placeholderLabel = UILabel().then {
+        $0.text = "메세지를 입력해주세요."
+        $0.font = UIFont.mogakcoFont.smallRegular
+        $0.textColor = .lightGray
+    }
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return .zero
+    }
+    
+    private func layout() {
+        backgroundColor = .white
+        addShadow(offset: .init(width: 1, height: 1))
+        
+        layoutMessageInputView()
+        configureMessageInputView()
+        layoutPlaceholder()
+    }
+    
+    private func layoutMessageInputView() {
+        addSubview(messageInputTextView)
+        
+        messageInputTextView.snp.makeConstraints {
+            $0.left.right.equalToSuperview().inset(16)
+            $0.top.equalToSuperview().inset(13)
+            $0.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).inset(8)
+        }
+    }
+    private let bag = DisposeBag()
+    
+    private func configureMessageInputView() {
+        messageInputTextView.rx.text.changed
+            .subscribe { [weak self] _ in
+                self?.placeholderLabel.isHidden = true
+            }
+            .disposed(by: bag)
+    }
+    
+    private func layoutPlaceholder() {
+        addSubview(placeholderLabel)
+        
+        placeholderLabel.snp.makeConstraints {
+            $0.centerY.equalTo(messageInputTextView)
+            $0.left.equalTo(messageInputTextView.snp.left).inset(8)
+        }
+    }
+}
