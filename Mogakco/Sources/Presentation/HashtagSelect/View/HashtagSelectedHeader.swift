b405a4bee29cc8d4bbfffc720d1cc02fae105ea1
Juhoon <wngns8334@gmail.com>
2022-11-17 01:22:21 +0900

    feat: Hashtag select ViewController 기본 UI 구성
    CollectionView의 레이아웃이 맞지 않아 수정 필요

---
+++ Mogakco/Sources/Presentation/HashtagSelect/View/HashtagSelectedHeader.swift
@@ -0,0 +1,18 @@
+//
+//  HashtagSelectedHeader.swift
+//  Mogakco
+//
+//  Created by 이주훈 on 2022/11/17.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
+
+final class HashtagSelectedHeader: UIView {
+    
+}
