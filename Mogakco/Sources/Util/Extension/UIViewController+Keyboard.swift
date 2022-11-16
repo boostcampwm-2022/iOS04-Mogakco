7e4b8cf3356c3a8fc8befced429d4a91ce480609
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-16 16:24:15 +0900

    feat: view 터치 시 Keyboard resign UIViewController Extension 추가

---
+++ Mogakco/Sources/Util/Extension/UIViewController+Keyboard.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Keyboard.swift
+//  Mogakco
+//
+//  Created by 김범수 on 2022/11/16.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
