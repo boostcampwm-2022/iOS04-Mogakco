fede1cf07e4c8958ad88b9aeba20c0e3860aec3b
oguuk <ogw135@gmail.com>
2022-11-17 00:23:04 +0900

    fix: 리뷰 반영
     Font 변경, disposeBag 위치 변경, 전송 button 추가, UI 변경

--- Mogakco/Sources/Presentation/Chat/View/CustomInputAccessoryView.swift
+++ Mogakco/Sources/Presentation/Chat/View/CustomInputAccessoryView.swift
@@ -13,14 +13,22 @@ import Then
 
 final class CustomInputAccessoryView: UIView {
     
+    private let bag = DisposeBag()
+    
     private let messageInputTextView = UITextView().then {
         $0.font = UIFont.systemFont(ofSize: 16)
         $0.textAlignment = .left
         $0.layer.cornerRadius = 15
         $0.layer.borderWidth = 1
         $0.layer.borderColor = UIColor.mogakcoColor.borderDefault?.cgColor
+        $0.textContainerInset = UIEdgeInsets(top: 8, left: 10, bottom: 8, right: 30)
         $0.isScrollEnabled = false
-        
+    }
+    
+    private let sendButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "arrow.up"), for: .normal)
+        $0.backgroundColor = .mogakcoColor.primaryDefault
+        $0.layer.cornerRadius = 12
     }
     
     private let placeholderLabel = UILabel().then {
@@ -45,8 +53,10 @@ final class CustomInputAccessoryView: UIView {
     private func layout() {
         backgroundColor = .white
         addShadow(offset: .init(width: 1, height: 1))
+        autoresizingMask = .flexibleHeight
         
         layoutMessageInputView()
+        layoutSendButton()
         configureMessageInputView()
         layoutPlaceholder()
     }
@@ -58,9 +68,18 @@ final class CustomInputAccessoryView: UIView {
             $0.left.right.equalToSuperview().inset(16)
             $0.top.equalToSuperview().inset(13)
             $0.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).inset(8)
+            $0.height.lessThanOrEqualTo(80)
         }
     }
-    private let bag = DisposeBag()
+    
+    private func layoutSendButton() {
+        addSubview(sendButton)
+        
+        sendButton.snp.makeConstraints {
+            $0.right.bottom.equalTo(messageInputTextView).inset(4)
+            $0.width.height.equalTo(25)
+        }
+    }
     
     private func configureMessageInputView() {
         messageInputTextView.rx.text.changed
