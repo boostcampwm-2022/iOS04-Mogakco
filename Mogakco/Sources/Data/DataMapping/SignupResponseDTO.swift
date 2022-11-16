2f5669519067c54885e1c70314d08a95d2652708
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-16 11:25:18 +0900

    feat: Signup 관련 DTO 모델 생성

---
+++ Mogakco/Sources/Data/DataMapping/SignupResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  SignupResponseDTO.swift
+//  Mogakco
+//
+//  Created by 김범수 on 2022/11/16.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+struct SignupResponseDTO: Decodable {
+}
