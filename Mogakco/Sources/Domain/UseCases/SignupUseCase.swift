d740135731e599584698851de9940d1a3ad755fc
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-18 14:25:33 +0900

    feat: SignupUseCase 회원가입 API 구현

---
+++ Mogakco/Sources/Domain/UseCases/SignupUseCase.swift
@@ -0,0 +1,33 @@
+//
+//  SignupUseCaseProtocol.swift
+//  Mogakco
+//
+//  Created by 김범수 on 2022/11/16.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import RxSwift
+
+protocol SignupUseCaseProtocol {
+    func signup(user: User) -> Observable<Void>
+}
+
+struct SignupUseCase: SignupUseCaseProtocol {
+    
+    private let authRepository: AuthRepositoryProtocol
+    private let userRepository: UserRepositoryProtocol
+    private let disposeBag = DisposeBag()
+    
+    init(
+        authRepository: AuthRepositoryProtocol,
+        userRepository: UserRepositoryProtocol
+    ) {
+        self.authRepository = authRepository
+        self.userRepository = userRepository
+    }
+    
+    func signup(user: User) -> Observable<Void> {
+        return authRepository.signup(user: user)
+            .flatMap { userRepository.save(user: $0) }
+    }
+}
