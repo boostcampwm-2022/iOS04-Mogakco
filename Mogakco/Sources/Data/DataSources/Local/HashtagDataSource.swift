5d863908be0d33094ae178c88ac32254c5b93175
Juhoon <wngns8334@gmail.com>
2022-11-19 16:05:34 +0900

    feat: Hashtag 언어 Repository부터 DataSource 생성

--- Mogakco/Sources/Data/DataSources/Local/HashtagDataSource.swift
+++ Mogakco/Sources/Data/DataSources/Local/HashtagDataSource.swift
@@ -8,10 +8,52 @@
 
 import Foundation
 
+import RxSwift
+
 struct HashtagDataSource: HashtagDataSourceProtocol {
-    func loadTagList(kind: KindHashtag) -> [String] {
-        // TODO: - 종류별로 리턴
+    func loadTagList(kind: KindHashtag) -> Observable<[String]> {
+        return Observable.create { emitter in
+            switch kind {
+            case .language: emitter.onNext(loadLanguage())
+            case .career: break
+            case .category: break
+            }
+            
+            return Disposables.create()
+        }
+    }
+    
+    private func loadLanguage() -> [String] {
+        return Languages.allNames()
+    }
+    
+    enum Languages: String, CaseIterable {
+        case cLang = "c"
+        case cShop
+        case cpp
+        case dart
+        case goLang = "go"
+        case haskell
+        case javaStript
+        case kotlin
+        case matlab
+        case objectC
+        case php
+        case python
+        case rLang = "r" // Lint 3자 제한 때문
+        case ruby
+        case rust
+        case scratch
+        case swift
+        case visualBasic
         
-        return []
+        static func allNames() -> [String] {
+            var names: [String] = []
+            self.allCases.forEach {
+                names.append($0.rawValue)
+            }
+            
+            return names
+        }
     }
 }
