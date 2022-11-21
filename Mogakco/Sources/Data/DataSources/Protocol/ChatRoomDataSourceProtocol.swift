3234fa12555dbe4ef3d6c09d05200269e9e0a09a
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-21 23:06:51 +0900

    feat: ChatRoomDataSource 전체 채팅방 목록 API 구현

---
+++ Mogakco/Sources/Data/DataSources/Protocol/ChatRoomDataSourceProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  ChatRoomDataSourceProtocol.swift
+//  Mogakco
+//
+//  Created by 김범수 on 2022/11/21.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import RxSwift
+
+protocol ChatRoomDataSourceProtocol {
+    func list() -> Observable<Documents<[ChatRoomResponseDTO]>>
+}
