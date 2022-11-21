e5a0318bd44f57e2bcda97fc7818572c9ea21a87
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-22 00:53:37 +0900

    feat: ProfileUseCase 유저 정보 조회 기능 구현

--- Mogakco/Sources/Data/DataMapping/UserResponseDTO.swift
+++ Mogakco/Sources/Data/DataMapping/UserResponseDTO.swift
@@ -11,16 +11,18 @@ import Foundation
 struct UserResponseDTO: Decodable {
     private let id: StringValue
     private let name: StringValue
+    private let introduce: StringValue
     private let email: StringValue
     private let languages: ArrayValue<StringValue>
     private let careers: ArrayValue<StringValue>
+    private let categorys: ArrayValue<StringValue>
     
     private enum RootKey: String, CodingKey {
         case fields
     }
     
     private enum FieldKeys: String, CodingKey {
-        case id, name, email, languages, careers
+        case id, name, introduce, email, languages, careers, categorys
     }
     
     init(from decoder: Decoder) throws {
@@ -28,27 +30,33 @@ struct UserResponseDTO: Decodable {
         let fieldContainer = try container.nestedContainer(keyedBy: FieldKeys.self, forKey: .fields)
         self.id = try fieldContainer.decode(StringValue.self, forKey: .id)
         self.name = try fieldContainer.decode(StringValue.self, forKey: .name)
+        self.introduce = try fieldContainer.decode(StringValue.self, forKey: .introduce)
         self.email = try fieldContainer.decode(StringValue.self, forKey: .email)
         self.languages = try fieldContainer.decode(ArrayValue<StringValue>.self, forKey: .languages)
         self.careers = try fieldContainer.decode(ArrayValue<StringValue>.self, forKey: .careers)
+        self.categorys = try fieldContainer.decode(ArrayValue<StringValue>.self, forKey: .categorys)
     }
     
     init(user: User) {
         self.id = StringValue(value: user.id ?? "")
         self.name = StringValue(value: user.name)
+        self.introduce = StringValue(value: user.introduce)
         self.email = StringValue(value: user.email)
         self.languages = ArrayValue<StringValue>(values: user.languages.map { StringValue(value: $0) })
         self.careers = ArrayValue<StringValue>(values: user.careers.map { StringValue(value: $0) })
+        self.categorys = ArrayValue<StringValue>(values: user.categorys.map { StringValue(value: $0) })
     }
     
     func toDomain() -> User {
         return User(
             id: id.value,
             email: email.value,
+            introduce: introduce.value,
             password: nil,
             name: name.value,
             languages: languages.arrayValue.map { $0.value }.flatMap { $0.map { $0.value } },
-            careers: careers.arrayValue.map { $0.value }.flatMap { $0.map { $0.value } }
+            careers: careers.arrayValue.map { $0.value }.flatMap { $0.map { $0.value } },
+            categorys: categorys.arrayValue.map { $0.value }.flatMap { $0.map { $0.value } }
         )
     }
 }
--- Mogakco/Sources/Domain/Repositories/UserRepositoryProtocol.swift
+++ Mogakco/Sources/Domain/Repositories/UserRepositoryProtocol.swift
@@ -11,4 +11,5 @@ import RxSwift
 protocol UserRepositoryProtocol {
     func save(user: User) -> Observable<Void>
     func user(id: String) -> Observable<User>
+    func load() -> Observable<User>
 }
