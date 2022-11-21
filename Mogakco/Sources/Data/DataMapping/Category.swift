1b68ed5e3e916e20d63d61fc0348a38a1f7fda81
Juhoon <wngns8334@gmail.com>
2022-11-21 17:14:35 +0900

    feat: Career, Category 열거형 추가

---
+++ Mogakco/Sources/Data/DataMapping/Category.swift
@@ -0,0 +1,41 @@
+//
+//  Category.swift
+//  Mogakco
+//
+//  Created by 이주훈 on 2022/11/21.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable, Hashtag {
+    case artificialIntelligence = "ai"
+    case algorithm
+    case android
+    case backend
+    case bigdata
+    case contest
+    case computerScience = "cs"
+    case frontend
+    case hackathon
+    case iOS = "ios"
+    case interview
+    case machineLearning
+    
+    func hashtagTitle() -> String {
+        switch self {
+        case .artificialIntelligence: return "AI"
+        case .algorithm: return "알고리즘"
+        case .android: return "안드로이드"
+        case .backend: return "백엔드"
+        case .bigdata: return "빅데이터"
+        case .contest: return "공모전"
+        case .computerScience: return "CS"
+        case .frontend: return "프론트엔드"
+        case .hackathon: return "해커톤"
+        case .iOS: return "iOS"
+        case .interview: return "인터뷰"
+        case .machineLearning: return "머신러닝"
+        }
+    }
+}
