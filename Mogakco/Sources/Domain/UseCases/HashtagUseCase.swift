f7e0f7f433399d93bb1e81b519a068c135eebc37
Juhoon <wngns8334@gmail.com>
2022-11-21 13:40:46 +0900

    feat: UseCase 파일 구조, 네이밍 변경

--- Mogakco/Sources/Domain/UseCases/HashtagUseCase.swift
+++ Mogakco/Sources/Domain/UseCases/HashtagUseCase.swift
@@ -10,7 +10,7 @@ import Foundation
 
 import RxSwift
 
-struct HashtagUsecase: HashtagUsecaseProtocol {
+struct HashtagUsecase: HashtagUseCaseProtocol {
     
     let hashtagRepository: HashtagRepositoryProtocol
     
