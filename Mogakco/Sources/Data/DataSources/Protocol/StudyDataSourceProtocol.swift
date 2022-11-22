6915dbf1a5202086f602c8d9e740e404a45f4dd3
sominn9 <tlsthals3415@naver.com>
2022-11-22 13:42:43 +0900

    feat: StudyDataSource 생성

---
+++ Mogakco/Sources/Data/DataSources/Protocol/StudyDataSourceProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  StudyDataSourceProtocol.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/22.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import RxSwift
+
+protocol StudyDataSourceProtocol {
+    func list() -> Observable<Documents<[StudyResponseDTO]>>
+    func detail(studyID: String) -> Observable<StudyResponseDTO>
+}
