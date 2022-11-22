1a05de3160546fa3b8a93ad0328b9c9b3ab24af2
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-22 19:04:30 +0900

    feat: URL -> 이미지 변환 관련 Extension 추가

---
+++ Mogakco/Sources/Util/Extension/UIImageView+URL.swift
@@ -0,0 +1,23 @@
+//
+//  UIImageView+URL.swift
+//  Mogakco
+//
+//  Created by 김범수 on 2022/11/22.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
