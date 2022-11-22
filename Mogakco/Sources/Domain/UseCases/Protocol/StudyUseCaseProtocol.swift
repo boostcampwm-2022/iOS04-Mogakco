b84079d0d555c83b8113d9f4c67f7848db8ce6e6
sominn9 <tlsthals3415@naver.com>
2022-11-22 14:25:39 +0900

    feat: StudyUseCase 생성

---
+++ Mogakco/Sources/Domain/UseCases/Protocol/StudyUseCaseProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  StudyUseCaseProtocol.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/22.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import RxSwift
+
+protocol StudyUseCaseProtocol {
+    func list() -> Observable<[Study]>
+    func detail(studyID: String) -> Observable<Study>
+}
