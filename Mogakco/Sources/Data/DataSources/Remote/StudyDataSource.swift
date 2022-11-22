54477cb74b357946c4408146eaee78d10983522a
sominn9 <tlsthals3415@naver.com>
2022-11-22 13:42:43 +0900

    feat: StudyDataSource 생성

---
+++ Mogakco/Sources/Data/DataSources/Remote/StudyDataSource.swift
@@ -0,0 +1,72 @@
+//
+//  StudyDataSource.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/22.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Alamofire
+import RxSwift
+
+struct StudyDataSource: StudyDataSourceProtocol {
+
+    private let provider: ProviderProtocol
+    
+    init(provider: ProviderProtocol) {
+        self.provider = provider
+    }
+
+    func list() -> Observable<Documents<[StudyResponseDTO]>> {
+        return provider.request(StudyTarget.list)
+    }
+    
+    func detail(studyID: String) -> Observable<StudyResponseDTO> {
+        return provider.request(StudyTarget.detail(studyID))
+    }
+}
+
+enum StudyTarget {
+    case list
+    case detail(String)
+}
+
+extension StudyTarget: TargetType {
+    var baseURL: String {
+        return "https://firestore.googleapis.com/v1/projects/mogakco-72df7/databases/(default)/documents/Study"
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var header: HTTPHeaders {
+        switch self {
+        case .list, .detail:
+            return ["Content-Type": "application/json"]
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .detail(let studyID):
+            return "/\(studyID)"
+        default:
+            return ""
+        }
+    }
+    
+    var parameters: RequestParams? {
+        switch self {
+        case .list, .detail:
+            return nil
+        }
+    }
+
+    var encoding: ParameterEncoding {
+        switch self {
+        case .list, .detail:
+            return JSONEncoding.default
+        }
+    }
+}
