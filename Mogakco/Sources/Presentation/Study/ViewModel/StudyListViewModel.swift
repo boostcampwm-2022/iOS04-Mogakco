a681fba4bcbfa9a5d5c1056fb39d0024239281da
sominn9 <tlsthals3415@naver.com>
2022-11-22 16:41:00 +0900

    feat: 컬렉션뷰에서 헤더 분리

---
+++ Mogakco/Sources/Presentation/Study/ViewModel/StudyListViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  StudyListViewModel.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/22.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+import RxCocoa
+import RxSwift
+
+final class StudyListViewModel: ViewModel {
+   
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    private weak var coordinator: StudyTabCoordinatorProtocol?
+    private let useCase: StudyListUseCaseProtocol
+    var disposeBag = DisposeBag()
+    
+    init(
+        coordinator: StudyTabCoordinatorProtocol,
+        useCase: StudyListUseCaseProtocol
+    ) {
+        self.coordinator = coordinator
+        self.useCase = useCase
+    }
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
