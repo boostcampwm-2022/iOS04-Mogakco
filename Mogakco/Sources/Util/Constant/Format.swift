6465123b5b06c733120ec0a9573b813e30087f92
sominn9 <tlsthals3415@naver.com>
2022-11-23 00:12:24 +0900

    refactor: dateFormat을 파라미터로 입력받도록 변경

---
+++ Mogakco/Sources/Util/Constant/Format.swift
@@ -0,0 +1,14 @@
+//
+//  Format.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/23.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+enum Format {
+    static let compactDateFormat: String = "yyyyMMddHHdd"
+    static let detailDateFormat: String = "yyyy년 MM월 dd일 HH시 dd분"
+}
