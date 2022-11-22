2048f6082317b50e3d92450ff85d15c232bb2d06
oguuk <ogw135@gmail.com>
2022-11-22 15:55:22 +0900

    refactor: viewModel 주입

--- Mogakco/Sources/Presentation/Common/Coordinator/StudyTabCoordinator.swift
+++ Mogakco/Sources/Presentation/Common/Coordinator/StudyTabCoordinator.swift
@@ -44,7 +44,8 @@ final class StudyTabCoordinator: Coordinator, StudyTabCoordinatorProtocol {
     }
     
     func showChatDetail() {
-        let chatViewController = ChatViewController()
+        let viewModel = ChatViewModel(coordinator: self)
+        let chatViewController = ChatViewController(viewModel: viewModel)
         navigationController.pushViewController(chatViewController, animated: true)
     }
 }
