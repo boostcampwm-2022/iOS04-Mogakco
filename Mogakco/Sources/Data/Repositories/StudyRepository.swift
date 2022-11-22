3cc543c8766977ec867a31e605db4169c25a5133
sominn9 <tlsthals3415@naver.com>
2022-11-22 14:21:15 +0900

    feat: StudyRepository 생성

---
+++ Mogakco/Sources/Data/Repositories/StudyRepository.swift
@@ -0,0 +1,29 @@
+//
+//  StudyRepository.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/22.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import RxSwift
+
+struct StudyRepository: StudyRepositoryProtocol {
+
+    private let dataSource: StudyDataSourceProtocol
+    private let disposeBag = DisposeBag()
+    
+    init(dataSource: StudyDataSourceProtocol) {
+        self.dataSource = dataSource
+    }
+    
+    func list() -> Observable<[Study]> {
+        return dataSource.list()
+            .map { $0.documents.map { $0.toDomain() } }
+    }
+    
+    func detail(studyID: String) -> Observable<Study> {
+        return dataSource.detail(studyID: studyID)
+            .map { $0.toDomain() }
+    }
+}
