ab2607cc1961955803d66e40a08e377cf4948932
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-22 11:10:08 +0900

    refactor: UserDatSource -> RetmoteUserDataSource 변수명 변경

--- Mogakco/Sources/Data/DataSources/Protocol/RemoteUserDataSourceProtocol.swift
+++ Mogakco/Sources/Data/DataSources/Protocol/RemoteUserDataSourceProtocol.swift
@@ -1,5 +1,5 @@
 //
-//  UserDataSourceProtocol.swift
+//  RemoteUserDataSourceProtocol.swift
 //  Mogakco
 //
 //  Created by 김범수 on 2022/11/21.
@@ -8,6 +8,6 @@
 
 import RxSwift
 
-protocol UserDataSourceProtocol {
+protocol RemoteUserDataSourceProtocol {
     func user(request: UserRequestDTO) -> Observable<UserResponseDTO>
 }
