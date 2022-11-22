5c25fbaf45502962fcfc5fca4bcd53e4a2ed5ea6
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-22 19:02:20 +0900

    feat: RemoteUserDataSource 프로필 이미지 업로드 API 구현

--- Mogakco/Sources/Data/DataSources/Protocol/RemoteUserDataSourceProtocol.swift
+++ Mogakco/Sources/Data/DataSources/Protocol/RemoteUserDataSourceProtocol.swift
@@ -6,9 +6,12 @@
 //  Copyright © 2022 Mogakco. All rights reserved.
 //
 
+import Foundation
+
 import RxSwift
 
 protocol RemoteUserDataSourceProtocol {
     func user(request: UserRequestDTO) -> Observable<UserResponseDTO>
     func editProfile(id: String, request: EditProfileRequestDTO) -> Observable<UserResponseDTO>
+    func uploadProfileImage(id: String, imageData: Data) -> Observable<URL>
 }
