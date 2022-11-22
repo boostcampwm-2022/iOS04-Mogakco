2aec66d0087472f70e4ebd91db239508beb0b510
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-22 19:04:30 +0900

    feat: URL -> 이미지 변환 관련 Extension 추가

---
+++ Mogakco/Sources/Util/Extension/RxUIImageView+URL.swift
@@ -0,0 +1,19 @@
+//
+//  RxUIImageView+URL.swift
+//  Mogakco
+//
+//  Created by 김범수 on 2022/11/22.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+
+extension Reactive where Base: UIImageView {
+    var loadImage: Binder<URL> {
+        return Binder(base) { base, url in
+            base.load(url: url)
+        }
+    }
+}
