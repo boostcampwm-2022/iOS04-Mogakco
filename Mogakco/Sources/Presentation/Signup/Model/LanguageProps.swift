820fa133238ef83f56025fdf3c3da150a88898be
sominn9 <tlsthals3415@naver.com>
2022-11-23 14:21:15 +0900

    feat: Props 모델 생성

---
+++ Mogakco/Sources/Presentation/Signup/Model/LanguageProps.swift
@@ -0,0 +1,18 @@
+//
+//  LanguageProps.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/23.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+struct LanguageProps {
+    let email: String
+    let password: String
+    let name: String
+    let introduce: String
+    let profileImageURLString: String
+    let languages: [String]
+}
