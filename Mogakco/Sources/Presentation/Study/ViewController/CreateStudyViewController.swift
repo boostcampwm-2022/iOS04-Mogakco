e8e345c93033812e6e20d13beed8507f75331e11
sominn9 <tlsthals3415@naver.com>
2022-11-23 12:29:58 +0900

    feat: 스터디 생성 화면 UI 2

--- Mogakco/Sources/Presentation/Study/ViewController/CreateStudyViewController.swift
+++ Mogakco/Sources/Presentation/Study/ViewController/CreateStudyViewController.swift
@@ -15,41 +15,84 @@ import Then
 
 final class CreateStudyViewController: ViewController {
     
+    enum Constant {
+        static let navigationTitle = "스터디 생성하기"
+        static let textFieldHeight = Layout.textFieldHeight + 38
+        static let textViewHeight = 120
+        static let selectViewHeight = 40
+        static let collectionViewHeight = 100
+    }
+    
+    private let scrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = false
+        $0.clipsToBounds = true
+    }
+    
     private let titleTextField = CountTextField().then {
         $0.title = "제목"
         $0.placeholder = "제목을 입력해주세요"
+        $0.snp.makeConstraints {
+            $0.height.equalTo(Constant.textFieldHeight)
+        }
     }
     
     private let contentTextView = CountTextView().then {
         $0.title = "내용"
         $0.placeholder = "내용을 입력해주세요"
+        $0.snp.makeConstraints {
+            $0.height.equalTo(Constant.textViewHeight)
+        }
     }
     
     private let placeTextField = CountTextField().then {
         $0.title = "지역"
         $0.placeholder = "지역을 입력해주세요"
+        $0.snp.makeConstraints {
+            $0.height.equalTo(Constant.textFieldHeight)
+        }
     }
-    
-    // MARK: 인원
     
     private let countStepper = StudyStepperView().then {
         $0.title = "인원"
+        $0.snp.makeConstraints {
+            $0.height.equalTo(Constant.selectViewHeight)
+        }
     }
     
-    // MARK: 날짜
-    
-    // MARK: 카테고리
-    
-    // MARK: 언어
-    
-    private let language = HashtagListView().then {
+    private let dateSelect = StudySelectView().then {
+        $0.title = "날짜"
         $0.snp.makeConstraints {
-            $0.height.equalTo(100.0)
+            $0.height.equalTo(Constant.selectViewHeight)
+        }
+    }
+    
+    private let categorySelect = StudySelectView().then {
+        $0.title = "카테고리"
+        $0.snp.makeConstraints {
+            $0.height.equalTo(Constant.selectViewHeight)
+        }
+    }
+    
+    private let languageSelect = StudySelectView().then {
+        $0.title = "언어"
+        $0.snp.makeConstraints {
+            $0.height.equalTo(Constant.selectViewHeight)
+        }
+    }
+    
+    private lazy var languageCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: collectionViewLayout()
+    ).then {
+        $0.register(BadgeCell.self, forCellWithReuseIdentifier: BadgeCell.identifier)
+        $0.showsHorizontalScrollIndicator = false
+        $0.snp.makeConstraints {
+            $0.height.equalTo(Constant.collectionViewHeight)
         }
     }
     
     private let createButton = ValidationButton().then {
-        $0.setTitle("스터디 생성하기", for: .normal)
+        $0.setTitle(Constant.navigationTitle, for: .normal)
         $0.snp.makeConstraints {
             $0.height.equalTo(Layout.buttonHeight)
         }
@@ -68,7 +111,7 @@ final class CreateStudyViewController: ViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - Methods
+    // MARK: - Override
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -78,8 +121,6 @@ final class CreateStudyViewController: ViewController {
         super.viewWillAppear(true)
         title = "새로운 스터디"
         navigationController?.isNavigationBarHidden = false
-        navigationController?.navigationBar.backItem?.title = "이전"
-        navigationController?.navigationBar.tintColor = .mogakcoColor.primaryDefault
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -88,21 +129,60 @@ final class CreateStudyViewController: ViewController {
     }
     
     override func layout() {
-        layoutTotalStackView()
         layoutCreateButton()
+        layoutScrollView()
     }
     
-    private func layoutTotalStackView() {
-        let stackView = UIStackView().then {
+    override func bind() {
+        Observable.of(Array((0...0)))
+            .bind(to: self.languageCollectionView.rx.items(
+                cellIdentifier: BadgeCell.identifier,
+                cellType: BadgeCell.self
+            )) { _, badge, cell in
+
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    // MARK: - Methods
+    
+    private func layoutScrollView() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.top.left.right.equalTo(view.safeAreaLayoutGuide).inset(16)
+            $0.bottom.equalTo(createButton.snp.top).offset(-8)
+        }
+        
+        let stackView = createTotalStackView()
+        scrollView.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+    }
+    
+    private func createTotalStackView() -> UIStackView {
+        let subviews = [
+            titleTextField,
+            contentTextView,
+            placeTextField,
+            countStepper,
+            dateSelect,
+            categorySelect,
+            createLanguageStackView()
+        ]
+        let stackView = UIStackView(arrangedSubviews: subviews).then {
             $0.axis = .vertical
             $0.spacing = 20
         }
-        [titleTextField, contentTextView, placeTextField, countStepper, language].forEach {
-            stackView.addArrangedSubview($0)
-        }
-        view.addSubview(stackView)
-        stackView.snp.makeConstraints {
-            $0.left.right.top.equalTo(view.safeAreaLayoutGuide).inset(16)
+        return stackView
+    }
+    
+    private func createLanguageStackView() -> UIStackView {
+        let subviews = [languageSelect, languageCollectionView]
+        return UIStackView(arrangedSubviews: subviews).then {
+            $0.axis = .vertical
+            $0.spacing = 10
         }
     }
     
@@ -110,6 +190,30 @@ final class CreateStudyViewController: ViewController {
         view.addSubview(createButton)
         createButton.snp.makeConstraints {
             $0.left.right.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
+        }
+    }
+    
+    private func collectionViewLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { _, _ in
+            let item = NSCollectionLayoutItem(
+                layoutSize: .init(
+                    widthDimension: .estimated(50),
+                    heightDimension: .absolute(30)
+                )
+            )
+            let group = NSCollectionLayoutGroup.vertical(
+                layoutSize: .init(
+                    widthDimension: .estimated(50),
+                    heightDimension: .fractionalHeight(1)
+                ),
+                subitems: [item]
+            )
+            group.interItemSpacing = .fixed(16)
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = 16
+            section.orthogonalScrollingBehavior = .continuous
+            section.contentInsets = .init(top: 4, leading: 4, bottom: 4, trailing: 4)
+            return section
         }
     }
 }
