ed822c20dfb22f5f0050d442e60c169d20398adb
sominn9 <tlsthals3415@naver.com>
2022-11-23 14:21:15 +0900

    feat: Props 모델 생성

---
+++ Mogakco/Sources/Presentation/Signup/Model/EmailProps.swift
@@ -0,0 +1,13 @@
+//
+//  EmailProps.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/23.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+struct EmailProps {
+    let email: String
+}
