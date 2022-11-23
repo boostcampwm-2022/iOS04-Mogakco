ef7e9d44dfcfab8e5d71ada5ccc68bac83c79f90
sominn9 <tlsthals3415@naver.com>
2022-11-23 14:21:15 +0900

    feat: Props 모델 생성

---
+++ Mogakco/Sources/Presentation/Signup/Model/CareerProps.swift
@@ -0,0 +1,19 @@
+//
+//  CareerProps.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/23.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+struct CareerProps {
+    let email: String
+    let password: String
+    let name: String
+    let introduce: String
+    let profileImageURLString: String
+    let languages: [String]
+    let categorys: [String]
+}
