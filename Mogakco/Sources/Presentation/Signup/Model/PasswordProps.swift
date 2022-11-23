34595a7ffc0ec28c2bced4c8fa88bc57667f6b64
sominn9 <tlsthals3415@naver.com>
2022-11-23 14:21:15 +0900

    feat: Props 모델 생성

---
+++ Mogakco/Sources/Presentation/Signup/Model/PasswordProps.swift
@@ -0,0 +1,14 @@
+//
+//  PasswordProps.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/23.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+struct PasswordProps {
+    let email: String
+    let password: String
+}
---
+++ Mogakco/Sources/Presentation/Signup/Model/ProfileProps.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileProps.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/23.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+struct ProfileProps {
+    let email: String
+    let password: String
+    let name: String
+    let introduce: String
+    let profileImageURLString: String
+}
