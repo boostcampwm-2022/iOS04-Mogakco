176545da60696e61715c28da54e5f92255d38651
sominn9 <tlsthals3415@naver.com>
2022-11-24 15:20:28 +0900

    fix: Date 문자열 포맷팅 방법 수정

--- Mogakco/Sources/Util/Extension/Int+Formatter.swift
+++ Mogakco/Sources/Util/Extension/Int+Formatter.swift
@@ -10,22 +10,47 @@ import Foundation
 
 extension Int {
     
+    /// ex) 202212011700 ==> 2022년 12월 1일 17시 00분
     func toDateString() -> String {
-        
-        var date = self
-        
-        var nums: [Int] = [100, 100, 100, 100, 10000].map {
-            let remain = date % $0
-            date /= $0
+        var number = self
+        let date: [Int] = [100, 100, 100, 100, 10000].map {
+            let remain = number % $0
+            number /= $0
             return remain
         }
-        nums.reverse()
         
-        let all = zip(nums, ["년", "월", "일", "시", "분"])
+        let all = zip(date.reversed(), ["년", "월", "일", "시", "분"])
         
         let dateString = all.reduce("") { result, info in
-            let twoDigit = String(format: "%02d", info.0)
-            return "\(result) \(twoDigit)\(info.1)"
+            let (num, unit) = info
+            let twoDigit = String(format: "%02d", num)
+            return "\(result) \(twoDigit)\(unit)"
+        }
+        
+        return dateString.trimmingCharacters(in: [" "])
+    }
+    
+    /// ex) 202212011700 ==> 2022년 12월 1일 오후 5시
+    func toCompactDateString() -> String {
+        var number = self
+        let date: [Int] = [100, 100, 100, 100, 10000].map {
+            let remain = number % $0
+            number /= $0
+            return remain
+        }
+        
+        let all = zip(date.reversed(), ["년", "월", "일", "시", "분"])
+        
+        let dateString = all.reduce("") { result, info in
+            let (num, unit) = info
+            if unit == "분" && num == 0 { return result }
+            if unit == "시" {
+                let hour = num <= 12 ? num : num - 12
+                let ampm = num < 12 ? "오전" : "오후"
+                return "\(result) \(ampm) \(hour)\(unit)"
+            } else {
+                return "\(result) \(num)\(unit)"
+            }
         }
         
         return dateString.trimmingCharacters(in: [" "])
