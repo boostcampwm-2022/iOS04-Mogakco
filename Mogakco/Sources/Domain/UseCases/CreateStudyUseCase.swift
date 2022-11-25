f50fe0a4be240bceb8ecb2676fbdbf78415b4ce2
sominn9 <tlsthals3415@naver.com>
2022-11-26 02:17:13 +0900

    feat: CreateStudyUseCase에서 API 호출

--- Mogakco/Sources/Domain/UseCases/CreateStudyUseCase.swift
+++ Mogakco/Sources/Domain/UseCases/CreateStudyUseCase.swift
@@ -22,11 +22,21 @@ struct CreateStudyUseCase: CreateStudyUseCaseProtocol {
         self.userRepository = userRepository
     }
     
-    func create(study: Study) -> Observable<Study> {
+    func create(study: Study) -> Observable<Void> {
         var study = study
         return userRepository.load()
             .compactMap { $0.id }
             .map { study.userIDs.append($0) }
-            .flatMap { studyRepository.create(study: study) }
+            .flatMap { studyRepository.create(study: study) } // 1. 스터디 생성
+//            .flatMap { } // 2. 채팅방 생성
+            .flatMap { _ in userRepository.load() } // 3. 유저 업데이트
+            .flatMap {
+                userRepository.updateIDs(
+                    id: $0.id,
+                    chatRoomIDs: $0.chatRoomIDs + [study.id],
+                    studyIDs: $0.chatRoomIDs + [study.id]
+                )
+            }
+            .map { _ in return () }
     }
 }
