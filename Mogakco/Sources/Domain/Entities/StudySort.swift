ee9c5b727fd3fde1fad118df07a33b7a93aa6d61
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-25 13:52:20 +0900

    feat: 스터디 정렬, 필터 Entity 모델 생성

---
+++ Mogakco/Sources/Domain/Entities/StudySort.swift
@@ -0,0 +1,13 @@
+//
+//  StudySort.swift
+//  Mogakco
+//
+//  Created by 김범수 on 2022/11/25.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+enum StudySort {
+    case latest, oldest
+}
