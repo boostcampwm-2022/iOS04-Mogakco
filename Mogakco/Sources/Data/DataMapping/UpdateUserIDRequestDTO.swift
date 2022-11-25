a5d815d412b4539d398bf2326f350031e58c454c
sominn9 <tlsthals3415@naver.com>
2022-11-26 02:44:22 +0900

    feat: userIDs 필드 업데이트를 위한 DTO 생성

---
+++ Mogakco/Sources/Data/DataMapping/UpdateUserIDRequestDTO.swift
@@ -0,0 +1,31 @@
+//
+//  UpdateUserIDRequestDTO.swift.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/26.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+struct UpdateUserIDRequestDTO: Encodable {
+    private let userIDs: ArrayValue<StringValue>
+    
+    private enum RootKey: String, CodingKey {
+        case fields
+    }
+    
+    private enum FieldKeys: String, CodingKey {
+        case userIDs
+    }
+    
+    init(userIDs: [String]) {
+        self.userIDs = ArrayValue(values: userIDs.map { StringValue(value: $0) })
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: RootKey.self)
+        var fieldContainer = container.nestedContainer(keyedBy: FieldKeys.self, forKey: .fields)
+        try fieldContainer.encode(self.userIDs, forKey: .userIDs)
+    }
+}
