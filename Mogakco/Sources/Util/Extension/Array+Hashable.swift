6d33a6513416c59c2eeecee2cd1d75a1460cfcb4
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-11-25 12:05:50 +0900

    feat: 프로필 화면 스터디 Top3 데이터 바인딩

---
+++ Mogakco/Sources/Util/Extension/Array+Hashable.swift
@@ -0,0 +1,23 @@
+//
+//  Array+Hashable.swift
+//  Mogakco
+//
+//  Created by 김범수 on 2022/11/25.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+extension Array where Element: Hashable {
+    var countDictionary: [Element: Int] {
+        var dictionary: [Element: Int] = [:]
+        self.forEach { element in
+            if let count = dictionary[element] {
+                dictionary[element] = count + 1
+            } else {
+                dictionary[element] = 1
+            }
+        }
+        return dictionary
+    }
+}
