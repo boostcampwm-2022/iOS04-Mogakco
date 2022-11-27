a8de3aea00788bd61fa53f1e053cff8c54a256a5
oguuk <ogw135@gmail.com>
2022-11-28 00:35:07 +0900

    feat: ChatUseCase 구현

---
+++ Mogakco/Sources/Domain/UseCases/Protocol/ChatUseCaseProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  ChatUseCaseProtocol.swift
+//  Mogakco
+//
+//  Created by 오국원 on 2022/11/27.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import RxSwift
+
+protocol ChatUseCaseProtocol {
+    func fetch(chatRoomID: String) -> Observable<[Chat]>
+    func send(chat: Chat, to chatRoomID: String) -> Observable<Error?>
+    func myProfile() -> Observable<User> 
+}
