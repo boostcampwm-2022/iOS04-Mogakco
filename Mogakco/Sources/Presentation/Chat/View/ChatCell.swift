f52e92e71e25b14853b6bc66625d0c02f09b8656
oguuk <ogw135@gmail.com>
2022-11-28 00:37:01 +0900

    feat: prepareForReuse

--- Mogakco/Sources/Presentation/Chat/View/ChatCell.swift
+++ Mogakco/Sources/Presentation/Chat/View/ChatCell.swift
@@ -37,10 +37,17 @@ final class ChatCell: UICollectionViewCell, Identifiable {
     override init(frame: CGRect) {
         super.init(frame: frame)
         layout()
+        prepareForReuse()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        profileImageView.image = UIImage(systemName: "person")
+        textView.text = nil
     }
     
     private func layout() {
