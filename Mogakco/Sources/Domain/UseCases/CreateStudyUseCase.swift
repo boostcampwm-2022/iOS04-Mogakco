2cc7096bec94f5577a6178c476057d9f56be7df7
sominn9 <tlsthals3415@naver.com>
2022-11-28 19:24:05 +0900

    feat: 서버 로직 CreateStudyUseCase -> StudyRepository로 이동

--- Mogakco/Sources/Domain/UseCases/CreateStudyUseCase.swift
+++ Mogakco/Sources/Domain/UseCases/CreateStudyUseCase.swift
@@ -12,52 +12,13 @@ import Foundation
 class CreateStudyUseCase: CreateStudyUseCaseProtocol {
     
     private let studyRepository: StudyRepositoryProtocol
-    private let userRepository: UserRepositoryProtocol
-    private let chatRoomRepository: ChatRoomRepositoryProtocol
     private let disposeBag = DisposeBag()
     
-    init(
-        studyRepository: StudyRepositoryProtocol,
-        userRepository: UserRepositoryProtocol,
-        chatRoomRepository: ChatRoomRepositoryProtocol
-    ) {
+    init(studyRepository: StudyRepositoryProtocol) {
         self.studyRepository = studyRepository
-        self.userRepository = userRepository
-        self.chatRoomRepository = chatRoomRepository
     }
     
-    func create(study: Study) -> Observable<Void> {
-        
-        return Observable<Void>.create { [weak self] emitter in
-        
-            guard let self = self else { return Disposables.create() }
-            
-            let user = self.userRepository.load()
-            
-            let createStudy = user
-                .map { Study(study: study, userIDs: [$0.id]) }
-                .flatMap { self.studyRepository.create(study: $0) }
-            
-            let createChatRoom = user
-                .flatMap { user in
-                    self.chatRoomRepository.create(studyID: study.id, userIDs: [user.id])
-                }
-            
-            let updateUser = user
-                .flatMap { user in
-                    self.userRepository.updateIDs(
-                        id: user.id,
-                        chatRoomIDs: user.chatRoomIDs + [study.id],
-                        studyIDs: user.chatRoomIDs + [study.id]
-                    )
-                }
-            
-            Observable
-                .zip(createStudy, createChatRoom, updateUser)
-                .subscribe { _ in emitter.onNext(()) }
-                .disposed(by: self.disposeBag)
-            
-            return Disposables.create()
-        }
+    func create(study: Study) -> Observable<Study> {
+        return studyRepository.create(study: study)
     }
 }
