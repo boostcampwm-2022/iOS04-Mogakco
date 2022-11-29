517c55379ca042fc2ea236a5bbcdbcc951ff5549
sominn9 <tlsthals3415@naver.com>
2022-11-29 14:24:57 +0900

    feat: TokenRepository 구현

---
+++ Mogakco/Sources/Data/DataSources/Protocol/TokenRepositoryProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  AutoLoginRepositoryProtocol.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/29.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import RxSwift
+
+protocol TokenRepositoryProtocol {
+    func save(_ auth: Authorization) -> Observable<Authorization?>
+    func load(email: String) -> Observable<Authorization?>
+    func delete(_ auth: Authorization) -> Observable<Authorization?>
+}
