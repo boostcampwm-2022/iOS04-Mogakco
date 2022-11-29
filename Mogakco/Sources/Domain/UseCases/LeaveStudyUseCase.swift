c0e2506c7f4d8398a76d30c3feff98418100fdb0
Juhoon <wngns8334@gmail.com>
2022-11-29 12:39:35 +0900

    feat: User정보에서 스터디, 채팅방 삭제, 스터디 정보에서 UserID 삭제

--- Mogakco/Sources/Domain/UseCases/LeaveStudyUseCase.swift
+++ Mogakco/Sources/Domain/UseCases/LeaveStudyUseCase.swift
@@ -18,11 +18,44 @@ struct LeaveStudyUseCase: LeaveStudyUseCaseProtocol {
     func leaveStudy(id: String) -> Observable<Void> {
         return Observable.create { emitter in
             
+            // 0. User 정보 업데이트 받기
+            let userInfo = userRepository
+                .load()
+                .flatMap { userRepository.user(id: $0.id) }
+            
             // 1. User에서 정보 삭제
+            let updateUser = userInfo
+                .flatMap {
+                    userRepository.updateIDs(
+                        id: $0.id,
+                        chatRoomIDs: $0.chatRoomIDs.filter { $0 != id },
+                        studyIDs: $0.studyIDs.filter { $0 != id }
+                    )
+                }
+                .flatMap {
+                    userRepository.save(user: $0)
+                }
             
             // 2. Study에서 정보 삭제
+            let updateStudy = Observable
+                .zip(
+                    userInfo,
+                    studyRepository.detail(id: id)
+                )
+                .flatMap { user, study in
+                    studyRepository.updateIDs(
+                        id: study.id,
+                        userIDs: study.userIDs.filter { $0 != user.id }
+                    )
+                }
             
             // 3. ChatRoom에서 정보 삭제
+//            let updateChatRoom = Observable
+//                .zip(
+//                    userInfo,
+//                    chatRoomRepository.
+//                )
+            
             
             return Disposables.create()
         }
