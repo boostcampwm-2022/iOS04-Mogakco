4240c158e839b187e78550bece7cdb86abdc8df0
sominn9 <tlsthals3415@naver.com>
2022-11-29 11:45:53 +0900

    feat: onError로 에러 전달

--- Mogakco/Sources/Domain/UseCases/JoinStudyUseCase.swift
+++ Mogakco/Sources/Domain/UseCases/JoinStudyUseCase.swift
@@ -26,13 +26,11 @@ struct JoinStudyUseCase: JoinStudyUseCaseProtocol {
     func join(id: String) -> Observable<Void> {
         return Observable<Void>.create { emitter in
             studyRepository.join(id: id)
-                .subscribe { result in
-                    if result {
-                        emitter.onNext(())
-                    } else {
-                        emitter.onError(JoinStudyUseCaseError.max)
-                    }
-                }
+                .subscribe(onNext: {
+                    emitter.onNext($0)
+                }, onError: { _ in
+                    emitter.onError(JoinStudyUseCaseError.max)
+                })
                 .disposed(by: disposeBag)
             return Disposables.create()
         }
