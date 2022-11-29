7437c27332366d4652c2e98a9d6ec3e73c89b233
Juhoon <wngns8334@gmail.com>
2022-11-29 15:58:54 +0900

    fix: Study Detail에 참여인원이 제대로 표시되지 않던 버그 수정

--- Mogakco/Sources/Presentation/Study/ViewController/StudyDetailViewController.swift
+++ Mogakco/Sources/Presentation/Study/ViewController/StudyDetailViewController.swift
@@ -155,7 +155,8 @@ final class StudyDetailViewController: ViewController {
             .subscribe(onNext: { [weak self] in
                 self?.studyTitleLabel.text = $0.title
                 self?.dateView.textLabel.text = $0.date.toCompactDateString()
-                self?.participantsView.textLabel.text = "\($0.userIDs.count)/\($0.maxUserCount)"
+                self?.participantsView.textLabel.text = "\($0.userIDs.count)/\($0.maxUserCount) 참여"
+                self?.participantsInfoLabel.text = "\($0.userIDs.count)/\($0.maxUserCount) 참여"
                 self?.locationView.textLabel.text = $0.place
                 self?.studyInfoDescription.text = $0.content
             })
