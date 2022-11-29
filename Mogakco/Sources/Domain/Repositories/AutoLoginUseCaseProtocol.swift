4c80596fad24852b6952b08c52a2aaafccdc97c8
sominn9 <tlsthals3415@naver.com>
2022-11-29 16:05:13 +0900

    feat: AutoLoginUseCase 구현

---
+++ Mogakco/Sources/Domain/Repositories/AutoLoginUseCaseProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  AutoLoginUseCaseProtocol.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/11/29.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import RxSwift
+
+protocol AutoLoginUseCaseProtocol {
+    func load() -> Observable<Bool>
+}
