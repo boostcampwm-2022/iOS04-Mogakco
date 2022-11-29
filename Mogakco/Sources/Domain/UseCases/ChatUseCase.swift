2ebf68145deca8fc6af1b04e0468dea2c9f6237c
oguuk <ogw135@gmail.com>
2022-11-29 17:14:58 +0900

    feat: Chat에 User와 isFromCurrentUser 초기화

--- Mogakco/Sources/Domain/UseCases/ChatUseCase.swift
+++ Mogakco/Sources/Domain/UseCases/ChatUseCase.swift
@@ -21,12 +21,24 @@ struct ChatUseCase: ChatUseCaseProtocol {
         self.chatRepository = chatRepository
         self.userRepository = userRepository
     }
-
-
-    func fetch(chatRoomID: String) -> Observable<[Chat]> {
-        return chatRepository.fetch(chatRoomID: chatRoomID)
+    
+    func fetch(chatRoomID: String) -> Observable<Chat> {
+        return chatRepository
+            .fetch(chatRoomID: chatRoomID)
+            .flatMap {
+                var chat = $0
+                return Observable.combineLatest(
+                    userRepository.user(id: chat.userID),
+                    userRepository.load()
+                )
+                .map { chatUserData, myUserData in
+                    chat.user = chatUserData
+                    chat.isFromCurrentUser = chatUserData.id == myUserData.id
+                    return chat
+                }
+            }
     }
-
+    
     func send(chat: Chat, to chatRoomID: String) -> Observable<Void> {
         return chatRepository.send(chat: chat, to: chatRoomID)
     }
