ce667857b3b62c4669eeed35bbe0e512413681b3
oguuk <ogw135@gmail.com>
2022-12-01 01:18:29 +0900

    feat: chatDateFormat 추가

--- Mogakco/Sources/Util/Constant/Format.swift
+++ Mogakco/Sources/Util/Constant/Format.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 enum Format {
+    static let chatDateFormat: String = "yyyyMMddHHmmss"
     static let compactDateFormat: String = "yyyyMMddHHmm"
     static let detailDateFormat: String = "yyyy년 MM월 dd일 HH시 mm분"
 }
