bfa921e1ee49ae25ed81244633f15424b14c7c11
Juhoon <wngns8334@gmail.com>
2022-12-01 15:08:46 +0900

    feat: autoLogin 화면에 애니메이션 뷰 추가

--- Mogakco/Sources/App/AppDelegate.swift
+++ Mogakco/Sources/App/AppDelegate.swift
@@ -13,9 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions:
-                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         FirebaseApp.configure()
         
         return true
