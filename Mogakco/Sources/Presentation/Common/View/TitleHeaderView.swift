8a99f2b5b36cd201d0f2324504ec8ecc8b0c7d61
oguuk <ogw135@gmail.com>
2022-12-06 01:24:49 +0900

    feat: 설정 버튼 표시

--- Mogakco/Sources/Presentation/Common/View/TitleHeaderView.swift
+++ Mogakco/Sources/Presentation/Common/View/TitleHeaderView.swift
@@ -18,6 +18,11 @@ final class TitleHeaderView: UIView {
         $0.textColor = UIColor.mogakcoColor.typographyPrimary
     }
     
+    let settingButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "info.circle"), for: .normal)
+        $0.tintColor = .mogakcoColor.primaryDefault
+    }
+    
     init() {
         super.init(frame: .zero)
         layout()
@@ -33,6 +38,7 @@ final class TitleHeaderView: UIView {
 
     private func layout() {
         layoutTitleLabel()
+        layoutSettingButton()
     }
     
     private func layoutTitleLabel() {
@@ -40,6 +46,14 @@ final class TitleHeaderView: UIView {
         
         titleLabel.snp.makeConstraints {
             $0.edges.equalToSuperview().inset(16.0)
+        }
+    }
+    
+    private func layoutSettingButton() {
+        addSubview(settingButton)
+        
+        settingButton.snp.makeConstraints {
+            $0.right.equalToSuperview().inset(16)
         }
     }
 }
