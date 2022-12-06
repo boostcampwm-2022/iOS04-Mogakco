b6af6c7f3bcff935cd91ab924f8a8a2b64939cac
oguuk <ogw135@gmail.com>
2022-12-06 22:05:05 +0900

    refactor: chatrepository reload 메서드 삭제

--- Mogakco/Sources/Data/Repositories/ChatRepository.swift
+++ Mogakco/Sources/Data/Repositories/ChatRepository.swift
@@ -12,12 +12,8 @@ struct ChatRepository: ChatRepositoryProtocol {
     var chatDataSource: ChatDataSourceProtocol?
     private let disposeBag = DisposeBag()
 
-    func fetchAll(chatRoomID: String) -> Observable<[Chat]> {
-        return chatDataSource?.fetchAll(chatRoomID: chatRoomID).map { $0.map { $0.toDomain() } } ?? .empty()
-    }
-    
-    func reload(chatRoomID: String) -> Observable<[Chat]> {
-        return chatDataSource?.reload(chatRoomID: chatRoomID).map { $0.map { $0.toDomain() } } ?? .empty()
+    func fetch(chatRoomID: String) -> Observable<[Chat]> {
+        return chatDataSource?.fetch(chatRoomID: chatRoomID).map { $0.map { $0.toDomain() } } ?? .empty()
     }
     
     func observe(chatRoomID: String) -> Observable<Chat> {
