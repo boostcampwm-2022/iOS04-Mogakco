483b6ecf741be948299239f562343caa9571548a
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-12-06 18:06:32 +0900

    docs: PushNotificationServiceProtocol API 주석 추가

--- Mogakco/Sources/Data/DataSources/Protocol/PushNotificationServiceProtocol.swift
+++ Mogakco/Sources/Data/DataSources/Protocol/PushNotificationServiceProtocol.swift
@@ -9,7 +9,10 @@
 import RxSwift
 
 protocol PushNotificationServiceProtocol {
+    // FCM Token을 이용하여 특정 유저에게 푸쉬 알림을 보내는 API
     func send(request: PushNotificationRequestDTO) -> Observable<EmptyResponse>
+    // 특정 Topic을 구독하고 있는 유저들에게 푸쉬 알림을 보내는 API
     func sendTopic(request: PushNotificationRequestDTO) -> Observable<EmptyResponse>
+    // Topic을 구독하는 API
     func subscribeTopic(topic: String)
 }
