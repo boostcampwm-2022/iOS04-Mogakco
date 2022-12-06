cca2daf816ecc84c7ec2a78851bfd7b58c37e88d
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-12-06 22:10:12 +0900

    style: PushNotificationRequestDTO 이니셜라이져 변경

--- Mogakco/Sources/Data/DataMapping/PushNotificationRequestDTO.swift
+++ Mogakco/Sources/Data/DataMapping/PushNotificationRequestDTO.swift
@@ -15,4 +15,14 @@ struct PushNotificationRequestDTO: Encodable {
     }
     let to: String
     let notification: Notification
+    
+    init(token: String, title: String, body: String) {
+        self.to = token
+        self.notification = .init(title: title, body: body)
+    }
+    
+    init(topic: String, title: String, body: String) {
+        self.to = "/topics/\(topic)"
+        self.notification = .init(title: title, body: body)
+    }
 }
