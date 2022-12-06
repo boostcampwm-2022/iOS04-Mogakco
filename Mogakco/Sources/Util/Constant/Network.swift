63998d95859acd29fcc74fc3d1054b2b8a890cb2
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-12-06 18:01:07 +0900

    feat: PushNotificationService 생성 및 푸쉬 알림 API 구현

--- Mogakco/Sources/Util/Constant/Network.swift
+++ Mogakco/Sources/Util/Constant/Network.swift
@@ -12,4 +12,6 @@ enum Network {
     static let baseURLString: String = "https://firestore.googleapis.com/v1/projects/mogakco-69b97/databases/(default)"
     static let authBaseURLString: String = "https://identitytoolkit.googleapis.com/v1"
     static let webAPIKey: String = "AIzaSyDZYeWGCGv_NZpqRrTiKfmlqbPD9nIGzJk"
+    static let fcmAPIKey: String = "AAAA6-cpVGg:APA91bHWQzHyJQspWpt1C3Mjm0M9ACaa4rw6LlL2kYPM_f41vEtb50fcUQY6QAX8a3CsRG28MVFgt2JFocl1TIpVyKq-gBIolEKIb55ahvpcopf2mcBNGR38jBRrBWqK19sj1Me9RGJJ"
+    static let fcmBaseURLStirng: String = "https://fcm.googleapis.com/fcm"
 }
