e5301ab93e3476ddec8fc9dcdb29bcb9069ac742
Juhoon <wngns8334@gmail.com>
2022-12-08 00:56:02 +0900

    faet: Skeleton 애니메이션 추가

---
+++ Mogakco/Sources/Presentation/Common/Skeleton/View/LoadingView.swift
@@ -0,0 +1,30 @@
+//
+//  LoadingView.swift
+//  Mogakco
+//
+//  Created by 이주훈 on 2022/12/08.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
+
+final class LoadingView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .backColor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        loadinAnimation()
+    }
+}
