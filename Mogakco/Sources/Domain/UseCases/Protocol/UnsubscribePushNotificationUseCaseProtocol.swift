62018c7443c2183ae3f93901dd31b5099cea0c40
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-12-07 17:04:59 +0900

    feat: PushNotification 구독, 해제 UseCase 구현

---
+++ Mogakco/Sources/Domain/UseCases/Protocol/UnsubscribePushNotificationUseCaseProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  UnsubscribePushNotificationUseCaseProtocol.swift
+//  Mogakco
+//
+//  Created by 김범수 on 2022/12/07.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import Foundation
+
+protocol UnsubscribePushNotificationUseCaseProtocol {
+    func excute(topic: String)
+}
