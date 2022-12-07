595e3c15bbe029cdaaf5d5c858249767d640e9f8
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-12-07 18:57:39 +0900

    fix: 프로필 해시태그 사이즈 조정

--- Mogakco/Sources/Presentation/Profile/View/HashtagListView.swift
+++ Mogakco/Sources/Presentation/Profile/View/HashtagListView.swift
@@ -29,7 +29,6 @@ final class HashtagListView: UIView {
         $0.setTitle("편집", for: .normal)
         $0.setTitleColor(UIColor.mogakcoColor.typographyPrimary, for: .normal)
         $0.titleLabel?.font = UIFont.mogakcoFont.caption
-        $0.setBackgroundColor(UIColor.mogakcoColor.primarySecondary ?? UIColor.white, for: .normal)
         $0.titleLabel?.textAlignment = .center
         $0.snp.makeConstraints {
             $0.size.equalTo(Constant.editButtonSize)
@@ -40,7 +39,8 @@ final class HashtagListView: UIView {
         frame: .zero,
         collectionViewLayout: UICollectionViewFlowLayout()).then {
         let layout = UICollectionViewFlowLayout()
-        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        layout.estimatedItemSize = CGSize(width: HashtagBadgeCell.addWidth, height: HashtagBadgeCell.height)
+        layout.itemSize = UICollectionViewFlowLayout.automaticSize
         layout.sectionInset = UIEdgeInsets(top: 0.0, left: 8.0, bottom: 0.0, right: 8.0)
 
         $0.collectionViewLayout = layout
@@ -49,8 +49,6 @@ final class HashtagListView: UIView {
         $0.bounces = false
         layout.scrollDirection = .horizontal
         $0.isPagingEnabled = false
-            $0.dataSource = nil
-            $0.delegate = nil
     }
     
     private let emptyLabel = UILabel().then {
@@ -83,7 +81,7 @@ final class HashtagListView: UIView {
                 return cell
             }
             .disposed(by: disposeBag)
-        
+
         hashtags
             .map { !$0.isEmpty }
             .drive(emptyLabel.rx.isHidden)
