839b89a36a154e4e9ced388df5bb87274d438a2b
sominn9 <tlsthals3415@naver.com>
2022-12-08 14:39:14 +0900

    style: small -> minimum 네이밍 변경

--- Mogakco/Sources/Util/Constant/Layout.swift
+++ Mogakco/Sources/Util/Constant/Layout.swift
@@ -9,9 +9,9 @@
 import Foundation
 
 enum Layout {
-    static let smallButtonHeight: CGFloat = 46
-    static let smallTextFieldHeight: CGFloat = 46
-    static let smallTextViewHeight: CGFloat = 54
+    static let minimumButtonHeight: CGFloat = 46
+    static let minimumTextFieldHeight: CGFloat = 46
+    static let minimumTextViewHeight: CGFloat = 54
     static let buttonHeight: CGFloat = 50
     static let textFieldHeight: CGFloat = 54
     static let textViewHeight: CGFloat = 54
