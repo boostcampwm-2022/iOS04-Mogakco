9498d2e2b2ae3208b7a38c960263d3fb7df8ea9d
sominn9 <tlsthals3415@naver.com>
2022-12-08 16:32:44 +0900

    feat: 이용약관 체크박스 View 생성

---
+++ Mogakco/Sources/Presentation/Signup/View/PolicyCheckBox.swift
@@ -0,0 +1,115 @@
+//
+//  PolicyCheckBox.swift
+//  Mogakco
+//
+//  Created by 신소민 on 2022/12/08.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+enum PolicyType: String {
+    case total = ""
+    case required = "필수"
+    case option = "선택"
+}
+
+final class PolicyCheckBox: UIView {
+    
+    let checkButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
+        $0.setImage(UIImage(systemName: "checkmark.circle.fill"), for: .selected)
+        $0.tintColor = UIColor.mogakcoColor.typopraphyDisabled
+    }
+    
+    private let bodyLabel = UILabel().then {
+        $0.font = UIFont(name: SFPro.regular.rawValue, size: 16)
+        $0.textColor = UIColor.mogakcoColor.typographyPrimary
+    }
+    
+    private let captionLabel = UILabel().then {
+        $0.font = UIFont.mogakcoFont.smallRegular
+        $0.textColor = UIColor.mogakcoColor.primaryDefault
+    }
+    
+    private let detailButton = UIButton().then {
+        $0.setTitle("자세히", for: .normal)
+        $0.titleLabel?.font = UIFont.mogakcoFont.smallRegular
+        $0.tintColor = UIColor.mogakcoColor.primaryDefault
+    }
+    
+    private let disposeBag = DisposeBag()
+    
+    private var link: String?
+    
+    // MARK: - Inits
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layout()
+        bind()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    
+    func setup(body: String, type: PolicyType, link: String? = nil) {
+        self.bodyLabel.text = body
+        self.captionLabel.text = type.rawValue
+        self.link = link
+        self.detailButton.isHidden = type == .total
+    }
+    
+    private func layout() {
+        let stackView = createTotalStackView()
+        addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func createTotalStackView() -> UIStackView {
+        let subviews = [checkButton, bodyLabel, captionLabel, UIView(), detailButton]
+        return UIStackView(arrangedSubviews: subviews).then {
+            $0.axis = .horizontal
+            $0.spacing = 10
+        }
+    }
+    
+    private func bind() {
+        checkButton.rx.tap
+            .withUnretained(self)
+            .map { !$0.0.checkButton.isSelected }
+            .bind(to: checkButton.rx.isSelected)
+            .disposed(by: disposeBag)
+        
+        detailButton.rx.tap
+            .withUnretained(self)
+            .subscribe(onNext: {
+                guard let link = $0.0.link,
+                      let url = URL(string: link),
+                      UIApplication.shared.canOpenURL(url) else {
+                    return
+                }
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+extension Reactive where Base: UIButton {
+    public var isSelected: ControlProperty<Bool> {
+        return base.rx.controlProperty(
+            editingEvents: [.touchUpInside],
+            getter: { $0.isSelected },
+            setter: { $0.isSelected = $1 })
+    }
+}
