b023f5f0fad7e5d4967ac98c3610661d705239f7
oguuk <ogw135@gmail.com>
2022-12-09 12:43:02 +0900

    feat: 채팅 시간 계산 및 표시

--- Mogakco/Sources/Util/Extension/Int+Formatter.swift
+++ Mogakco/Sources/Util/Extension/Int+Formatter.swift
@@ -55,4 +55,30 @@ extension Int {
         
         return dateString.trimmingCharacters(in: [" "])
     }
+    
+    /// ex) 202212011700 ==> 오후 5시
+    func toChatCompactDateString() -> String {
+        var number = self / 100
+        let date: [Int] = [100, 100].map {
+            let remain = number % $0
+            number /= $0
+            return remain
+        }
+        
+        let all = zip(date.reversed(), ["시", "분"])
+        
+        let dateString = all.reduce("") { result, info in
+            let (num, unit) = info
+            if unit == "분" && num == 0 { return result }
+            if unit == "시" {
+                let hour = num <= 12 ? num : num - 12
+                let ampm = num < 12 ? "오전" : "오후"
+                return "\(result) \(ampm) \(hour)\(unit)"
+            } else {
+                return "\(result) \(num)\(unit)"
+            }
+        }
+        
+        return dateString.trimmingCharacters(in: [" "])
+    }
 }
