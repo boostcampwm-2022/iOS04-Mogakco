25db6425c00b578b51c18162463ec1259a1dea28
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-12-12 12:53:35 +0900

    feat: 채팅방 목록 푸쉬 알림 도착 시 리로드 및

--- Mogakco/Sources/App/AppDelegate.swift
+++ Mogakco/Sources/App/AppDelegate.swift
@@ -101,6 +101,7 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        ChatListViewModel.reload.onNext(())
         completionHandler([[.banner, .badge, .sound]])
     }
     
