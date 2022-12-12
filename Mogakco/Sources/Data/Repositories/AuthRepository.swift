a31402b644c2b6e81453fe4a82ccceaecfb73ee2
beomsoo0 <73675540+beomsoo0@users.noreply.github.com>
2022-12-12 15:58:34 +0900

    feat: 로그인 시 채팅방 푸쉬 알림 구독 기능 구현

--- Mogakco/Sources/Data/Repositories/AuthRepository.swift
+++ Mogakco/Sources/Data/Repositories/AuthRepository.swift
@@ -11,6 +11,9 @@ import RxSwift
 struct AuthRepository: AuthRepositoryProtocol {
     
     var authService: AuthServiceProtocol?
+    var remoteUserDataSource: RemoteUserDataSourceProtocol?
+    var chatRoomDataSource: ChatRoomDataSourceProtocol?
+    var pushNotificationService: PushNotificationServiceProtocol?
     private let disposeBag = DisposeBag()
     
     func signup(signupProps: SignupProps) -> Observable<Authorization> {
@@ -21,7 +24,27 @@ struct AuthRepository: AuthRepositoryProtocol {
     
     func login(emailLogin: EmailLogin) -> Observable<Authorization> {
         let request = EmailAuthorizationRequestDTO(emailLogin: emailLogin)
-        return authService?.login(request)
-            .map { $0.toDomain() } ?? .empty()
+        return (authService?.login(request) ?? .empty())       // 로그인
+            .map { $0.toDomain() }
+            .flatMap { authorization in  // 유저 채팅방 푸쉬 알림 구독
+                return (remoteUserDataSource?.user(id: authorization.localId) ?? .empty())
+                    .map { $0.toDomain() }
+                    .map { $0.chatRoomIDs }
+                    .flatMap { chatRoomIDs in
+                        guard !chatRoomIDs.isEmpty else {
+                            return Observable.just(())
+                        }
+                        return (chatRoomDataSource?.list() ?? .empty())
+                            .map { $0.documents.map { $0.toDomain() } }
+                            .map { $0.filter { chatRoomIDs.contains($0.id) } }
+                            .flatMap { chatRooms in
+                                return Observable.combineLatest(chatRooms.map {
+                                    pushNotificationService?.subscribeTopic(topic: $0.id) ?? .empty()
+                                })
+                            }
+                            .map { _ in () }
+                    }
+                    .map { _ in authorization }
+            }
     }
 }
