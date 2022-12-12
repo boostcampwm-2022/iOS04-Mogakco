1a99a4d7f12aac2f3ff0c481346817e0237c45c4
sominn9 <tlsthals3415@naver.com>
2022-12-12 17:20:25 +0900

    feat: 신고 메뉴 destructive로 설정

--- Mogakco/Sources/Presentation/Chat/View/ChatCell.swift
+++ Mogakco/Sources/Presentation/Chat/View/ChatCell.swift
@@ -44,10 +44,11 @@ final class ChatCell: UICollectionViewCell, Identifiable {
             UIAction(
                 title: menu.title,
                 image: menu.image,
+                attributes: menu.attributes,
                 handler: { _ in self?.menuSelected.onNext(menu) }
             )
         }
-        $0.menu = UIMenu(options: .destructive, children: actions)
+        $0.menu = UIMenu(options: .displayInline, children: actions)
     }
     
     let menuSelected = PublishSubject<ChatMenu>()
@@ -216,6 +217,13 @@ enum ChatMenu: CaseIterable {
         switch self {
         case .report:
             return UIImage(systemName: "exclamationmark.circle")
+        }
+    }
+    
+    var attributes: UIMenuElement.Attributes {
+        switch self {
+        case .report:
+            return .destructive
         }
     }
 }
