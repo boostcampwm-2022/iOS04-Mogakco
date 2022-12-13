491108421c12aa3d368277ed002b4795f405a97d
oguuk <ogw135@gmail.com>
2022-12-14 01:10:02 +0900

    feat: Cache사이즈 설정

---
+++ Mogakco/Sources/Util/Constant/CacheConstants.swift
@@ -0,0 +1,13 @@
+//
+//  CacheConstants.swift
+//  Mogakco
+//
+//  Created by 오국원 on 2022/12/06.
+//  Copyright © 2022 Mogakco. All rights reserved.
+//
+import Foundation
+
+enum CacheConstants {
+    static let maximumMemoryCacheSize = 52428800
+    static let maximumDiskCacheSize = 52428800
+}
