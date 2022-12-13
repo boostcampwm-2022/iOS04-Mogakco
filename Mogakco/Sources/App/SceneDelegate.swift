9d80d8debf4dedd2a2a38d868a680b3c88780a7a
oguuk <ogw135@gmail.com>
2022-12-14 01:10:42 +0900

    refactor: disposeBag 추가

--- Mogakco/Sources/App/SceneDelegate.swift
+++ Mogakco/Sources/App/SceneDelegate.swift
@@ -22,6 +22,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        DefaultImageCacheService.shared.configureCache(
+            with: CacheConstants.maximumMemoryCacheSize,
+            with: CacheConstants.maximumDiskCacheSize
+        )
         appCoordinator = AppCoordinator(window)
         appCoordinator?.start()
             .subscribe()
